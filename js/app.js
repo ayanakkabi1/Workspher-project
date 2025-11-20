fce3284ee1903817e22c107ea20338c1a44094b0
ayanakkabi1 <aynour200609@gmail.com>
2025-11-20 18:50:32 +0100

    feat: comit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const btn = document.getElementById("burgerBtn");
 btn.addEventListener("click", () => {
   btn.classList.toggle("active");
+  console.log('test');
 });
 function createemploye(employee){
    const div= document.createElement("div");
